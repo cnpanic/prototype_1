5341e6e3712226c3a06e35cc57ea1b89458b062f
Chu <cnpanic25@gamil.com>
2018-01-05 15:42:51 +0900

    Initial Commit

---
+++ mockup-0105/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mockup-0105
+//
+//  Created by Jung Ho Chu on 2018. 1. 5..
+//  Copyright © 2018년 Jung Ho Chu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
